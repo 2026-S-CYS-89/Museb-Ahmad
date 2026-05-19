241cdd3f862aec615c5eb79564edc1fe9e5c2365
2026-S-CYS-89 <musab863dfh4@gmail.com>
2026-05-19 12:37:32 +0500

    Add files via upload

---
+++ pf lab week 10 and 11/pf lab task 8.py
@@ -0,0 +1,13 @@
+
+i=0
+while i<=13:
+    j=0
+    while j<=(13-i):
+        print(" ",end="")
+        j+=1
+    k=0
+    while k<=i:
+        print("*",end=" ")
+        k+=1
+    print()
+    i=i+2
